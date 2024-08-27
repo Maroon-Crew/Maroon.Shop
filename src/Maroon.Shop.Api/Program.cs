6bec7d3e649f902eb2c0ace88d2b011f3c765a43
Matthew Jones <DoctaJonez@users.noreply.github.com>
2024-08-27 09:35:12 +0100

    Ignore cycles, so link backs are suppported

--- src/Maroon.Shop.Api/Program.cs
+++ src/Maroon.Shop.Api/Program.cs
@@ -1,11 +1,15 @@
 using Maroon.Shop.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers().AddJsonOptions(o =>
+{
+    o.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
