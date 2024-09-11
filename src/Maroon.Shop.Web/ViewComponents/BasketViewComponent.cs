6b6a75f4dd50c9f08a98116fa2a19719ba94f27a
Matthew Jones <DoctaJonez@users.noreply.github.com>
2024-09-11 13:52:41 +0100

    Add basket button to site layout

---
+++ src/Maroon.Shop.Web/ViewComponents/BasketViewComponent.cs
@@ -0,0 +1,30 @@
+﻿namespace Maroon.Shop.Web.ViewComponents
+{
+    using Maroon.Shop.Data;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+
+    public class BasketViewComponent : ViewComponent
+    {
+        private readonly ShopContext _context;
+
+        public BasketViewComponent(ShopContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var custromerId = 1;
+
+            var query = from b in _context.Baskets
+                        from i in b.Items
+                        where b.Customer.CustomerId == custromerId
+                        select i.Quantity;
+
+            int count = await query.SumAsync();
+
+            return View(count);
+        }
+    }
+}
